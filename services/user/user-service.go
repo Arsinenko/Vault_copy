c1e57b34f83add9783780b371ae19eaa28c6c128
zxcpadik <141325167+zxcpadik@users.noreply.github.com>
2024-10-14 17:06:37 +0300

    [0.0.9] Sctructure fixes

--- services/user/user-service.go
+++ services/user/user-service.go
@@ -159,32 +159,6 @@ func CreateUser(phone_mail string, password string, full_name string) int {
 	return http.StatusOK
 }
 
-func CreateApp(Name string, Description string, OwnerID int32, metadata pgtype.JSONB) int {
-	_log_hash := hex.EncodeToString(cryptoOperation.SHA256(append([]byte(Name + Description + string(OwnerID)), metadata.Bytes...)));
-	LogService.PushAuditLog(LogService.EventTryCreateApp, OwnerID, 0, 0, _log_hash);
-
-	db, e := db_operations.InitDB()
-	if e != nil {
-		LogService.Push_server_log(LogService.ErrorDBInit, LogService.TErrorDBInit, "[CreateApp]::db_operations.InitDB()", _log_hash)
-		return http.StatusInternalServerError
-	}
-	var app models.App
-	app.Name = Name
-	app.Description = Description
-	app.OwnerID = OwnerID
-	app.Metadata = metadata
-	app.APIPath = strings.ToLower(strings.ReplaceAll(Name, " ", "_"))
-	app.CreationDate = time.Now()
-	err := db.Create(&app).Error
-	if err != nil {
-		LogService.Push_server_log(LogService.ErrorCreateApp, LogService.TErrorCreateApp, "[CreateApp]::db.Create(&app)", _log_hash)
-		return http.StatusInternalServerError
-	}
-	LogService.PushAuditLog(LogService.EventCreateApp, app.OwnerID, app.ID, 0, _log_hash)
-
-	return http.StatusOK
-}
-
 func CreateSecret(SID string, Data []byte, AppID int32, Metadata pgtype.JSONB) int { // SID, ya hz ne pomny zachem eto)))))
 	_log_hash := hex.EncodeToString(append(cryptoOperation.SHA256(append([]byte(SID+string(AppID)), Data...)), Metadata.Bytes...));
 	LogService.PushAuditLog(LogService.EventTryCreateSecret, 0, AppID, 0, _log_hash);
